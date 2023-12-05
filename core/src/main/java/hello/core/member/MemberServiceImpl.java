86a6d6bd1efd4f50b2b5de6a46726ba4e4914d0c
2017tmxkxm <2017tmxkxm@gmail.com>
2023-12-05 15:06:33 +0900

    [Update] DIP, OCP 원칙 적용

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,15 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    /* DIP, OCP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    */
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
