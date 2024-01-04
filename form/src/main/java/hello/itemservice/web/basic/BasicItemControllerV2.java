eb93ed95c4cdcbd899e0dba98e48829df208ebe5
2017tmxkxm <2017tmxkxm@gmail.com>
2024-01-04 15:33:42 +0900

    [Update] 검증 분리 v1

--- form/src/main/java/hello/itemservice/web/basic/BasicItemControllerV2.java
+++ form/src/main/java/hello/itemservice/web/basic/BasicItemControllerV2.java
@@ -25,6 +25,7 @@ import java.util.*;
 public class BasicItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @ModelAttribute("regions")
     public Map<String, String> regions() {
@@ -174,7 +175,7 @@ public class BasicItemControllerV2 {
         return "redirect:/basic/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         // 검증 로직
@@ -195,6 +196,24 @@ public class BasicItemControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()) {
+            log.info("bindingResult ={}", bindingResult);
+            return "basic/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        itemValidator.validate(item, bindingResult);
 
         // 검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()) {
