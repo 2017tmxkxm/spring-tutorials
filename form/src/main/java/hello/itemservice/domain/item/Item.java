5c106df0026f3f6ac81266031652456320a9764a
2017tmxkxm <2017tmxkxm@gmail.com>
2024-01-06 12:46:19 +0900

    [Update] 애노테이션 주석처리

--- form/src/main/java/hello/itemservice/domain/item/Item.java
+++ form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,6 +3,7 @@ package hello.itemservice.domain.item;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
@@ -10,21 +11,21 @@ import org.hibernate.validator.constraints.ScriptAssert;
 
 import java.util.List;
 
-@Getter @Setter
+@Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+    //@NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     private Boolean open;           // 판매 여부
