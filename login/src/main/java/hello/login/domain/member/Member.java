7b8e0045e86dfefe31f867816f23223dfc9fcb91
2017tmxkxm <2017tmxkxm@gmail.com>
2024-01-07 19:55:26 +0900

    [Create] Member

---
+++ login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,26 @@
+package hello.login.domain.member;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long id;
+
+    @NotEmpty
+    private String loginId; //로그인 ID
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String password;
+
+    public Member() {
+    }
+
+    public Member(String loginId, String name, String password) {
+        this.loginId = loginId;
+        this.name = name;
+        this.password = password;
+    }
+}
