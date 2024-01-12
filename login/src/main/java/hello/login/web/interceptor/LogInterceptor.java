894a94f09c104d4e1b876b0f3a847b06061e5ade
2017tmxkxm <2017tmxkxm@gmail.com>
2024-01-12 17:13:09 +0900

    [Update] 로그 인터셉터

--- login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
+++ login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -27,16 +27,22 @@ public class LogInterceptor implements HandlerInterceptor {
         }
 
         log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+        return true;
 
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        log.info("postHandle [{}]", modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        Object logId = request.getAttribute(LOG_ID);
+        String requestURI = request.getRequestURI();
+        String logId = (String) request.getAttribute(LOG_ID);
+        log.info("REQUEST [{}][{}][{}]", logId, requestURI, handler);
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
     }
 }
