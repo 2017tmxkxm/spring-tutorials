1782e5d9150eda612d54588580fe88a220d7261f
2017tmxkxm <2017tmxkxm@gmail.com>
2024-01-26 14:13:54 +0900

    [Update] String, Integer Converter 주석 처리 및 Formatter 추가

--- typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.coverter.IntegerToStringConverter;
 import hello.typeconverter.coverter.IpPortToStringConverter;
 import hello.typeconverter.coverter.StringToIntegerConverter;
 import hello.typeconverter.coverter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        //registry.addConverter(new StringToIntegerConverter());
+        //registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        // Formatter 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
